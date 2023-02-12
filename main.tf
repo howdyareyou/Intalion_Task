3f9adfc10d8aeec8f02c055e3d6a13afb61d03db
howdyareyou <114950212+howdyareyou@users.noreply.github.com>
2023-02-12 14:53:33 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  profile = "vscode"
+
+}
+
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443, 22]
+}
+
+resource "aws_security_group" "web_traffic" {
+  name        = "Allow web traffic"
+  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# Define the EC2 instance resource.
+resource "aws_instance" "web_server" {
+  ami           = "ami-0067a5ac65b10cc84"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  key_name = "Hack"
+  
+  tags = {
+    Name = "ec2terraform-tag"
+  }
+}
+
+
